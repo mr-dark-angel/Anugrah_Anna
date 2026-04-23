8b61c859c5413ab90a947b7c384ee72c8a548596
Anugrah Anna <63781007+mr-dark-angel@users.noreply.github.com>
2026-04-23 14:34:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ toggle.addEventListener("click", () => {
     toggle.textContent = "🌙";
   }
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  const windowHeight = window.innerHeight;
+  reveals.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < windowHeight - 80) {
+      section.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
+``
